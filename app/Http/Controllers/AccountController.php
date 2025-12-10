4ea0c1b310e2870268f1cd0de180b1ad97618bb8
= <=>
2025-12-10 16:49:15 +0100

    Ajout du Contrôleur Compte

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+Class AccountController extends Controller {
+
+    $host = ;
+    $dbname = ;
+    $user = ;
+    $password = ;
+    $connexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+
+}
+
+?>
