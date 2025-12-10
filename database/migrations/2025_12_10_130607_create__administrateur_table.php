2c2869b83ab2260941bb4b9242e8f9dc69c85779
Diego <diego.alvesferreira.sio@gmail.com>
2025-12-10 14:33:08 +0100

    migration admin

--- database/migrations/2025_12_10_130607_create__administrateur_table.php
+++ database/migrations/2025_12_10_130607_create__administrateur_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('_administrateur', function (Blueprint $table) {
             $table->id();
+            $table->string('nom_utilisateur')->unique();
+            $table->string('email')->unique();
+            $table->string('mdp');
             $table->timestamps();
         });
     }
