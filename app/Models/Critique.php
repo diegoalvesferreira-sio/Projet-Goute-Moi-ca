6dd30b049df0f51e05082abb8e5ec8a70f105426
louaye-lacour-sio <louaye.lacour.sio@gmail.com>
2026-03-31 08:31:33 +0200

    Correction du Modèle de Critique

--- app/Models/Critique.php
+++ app/Models/Critique.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Critique extends Authenticatable
